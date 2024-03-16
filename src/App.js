89f3bf91b7266a5e8ba21d4683747f1996231fe5
Raj Aryan <100435391+raj4kri@users.noreply.github.com>
2024-03-16 20:38:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Home from './componenets/Home/Home.js';
+
+function App() {
+  return (
+  
+    <Home/>
+    
+  
+  );
+}
+
+export default App;
---
+++ src/componenets/Home/Home.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar';
+import Card from '../Card/Card';
+import Card2 from '../Card2/Card2';
+
+
+// import Search from '../Section/Section'
+import Section from '../Section/Section';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+export default function home() {
+  return (
+    <div>
+
+
+<div className="container">
+
+<Navbar/>
+
+<Section/>
+<Card/>
+<Card2/>
+
+</div>
+    
+
+ 
+
+        
+
+
+
+      
+    </div>
+  )
+}
